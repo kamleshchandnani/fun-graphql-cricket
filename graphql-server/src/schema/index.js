bd72361569ed9111b3d55ac47beb327196dee722
kamleshchandnani <kamlesh.chandnani@gmail.com>
2019-04-04 18:56:13 +0530

    docs(graphql-server): add schema gist

--- graphql-server/src/schema/index.js
+++ graphql-server/src/schema/index.js
@@ -1,3 +1,43 @@
+/**
+ * Schema
+ * Team {
+ * id: ID!
+ * name
+ * player: [Player!]!
+ * imageUrl
+ * }
+ *
+ * Player {
+ * id: ID!
+ * name: String!
+ * }
+ *
+ * Match {
+ * id: ID!
+ * teams: [Team!]!
+ * innings: [Inning!]!
+ * location: String!
+ * }
+ *
+ * Inning {
+ * battingTeam: [Team!]!
+ * bowlingTeam: [Team!]!
+ * score: Score!
+ * }
+ *
+ * Score {
+ * runs: Int!
+ * wickets: Int!
+ * overs: Float!
+ * }
+ *
+ * createTeam
+ * createMatch
+ * createInning
+ * updateScore
+ * getMatchUpdates -- subscription
+ */
+
 import merge from 'lodash/merge';
 import flatten from 'lodash/flatten';
 import { gql } from 'apollo-server-express';
